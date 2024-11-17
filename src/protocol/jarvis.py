f9c53b7aa39dc3ace2bbcb7120a4d8c0f7d9dac4
itzVort3x1 <saikaustubh10@gmail.com>
2024-11-17 15:55:00 +0000

    updated jarvis

--- src/protocol/jarvis.py
+++ src/protocol/jarvis.py
@@ -6,7 +6,7 @@ import struct
 
 
 class Jarvis:
-    def __init__(self, receive_port=33000, send_port=34000, adjacency_list_file="./protocol/discovery/adjacency_list.json"):
+    def __init__(self, receive_port=12345, send_port=12345, adjacency_list_file="./protocol/discovery/adjacency_list.json"):
         self.receive_port = receive_port
         self.send_port = send_port
         self.local_ip = self.get_local_ip()
