cbf5ad4264a3d5b6611428d60178f2e9de1404c1
Hari <reachk.hariharan@gmail.com>
2024-11-18 18:19:37 +0000

    testing gen

--- src/protocol/generate_nodes.py
+++ src/protocol/generate_nodes.py
@@ -8,6 +8,7 @@ import platform
 from tqdm import tqdm
 from jarvis_copy import Jarvis
 import string
+from ipaddress import IPv4Address
 
 # Configuration
 specific_port = 33000  # The port to scan
@@ -137,7 +138,10 @@ if __name__ == "__main__":
     #discovered_nodes.append(local_ip)
 
     print("discovery nodes", discovered_nodes)
-    valid_nodes = ["10.35.70.7","10.35.70.17","10.35.70.27","10.35.70.3","10.35.70.4","10.35.70.25","10.35.70.28"]
+    valid_nodes = [IPv4Address('10.35.70.7'), IPv4Address('10.35.70.17'), IPv4Address('10.35.70.27'),
+        IPv4Address('10.35.70.3'), IPv4Address('10.35.70.4'), IPv4Address('10.35.70.25'),
+        IPv4Address('10.35.70.28')]
+
     filtered_nodes = [node for node in discovered_nodes if node in valid_nodes]
     # Build adjacency list if nodes are discovered
     if filtered_nodes:
