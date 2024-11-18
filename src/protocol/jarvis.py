c717a53016200aedb134387890b5d3b5218c655c
Sai Kaustubh <saikaustubh10@gmail.com>
2024-11-18 17:10:19 +0000

    updated ports

--- src/protocol/jarvis.py
+++ src/protocol/jarvis.py
@@ -13,7 +13,7 @@ from cryptography.hazmat.primitives import serialization
 
 
 class Jarvis:
-    def __init__(self, receive_port=12345, send_port=12345, adjacency_list_file="./protocol/discovery/adjacency_list.json"):
+    def __init__(self, receive_port=33000, send_port=34000, adjacency_list_file="./protocol/discovery/adjacency_list.json"):
         self.receive_port = receive_port
         self.send_port = send_port
         self.local_ip = self.get_local_ip()
